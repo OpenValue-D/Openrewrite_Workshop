c80cebf05f923ec9caf3a3365673ef27aa1c52fa
Sascha Selzer <sascha.selzer@gmail.com>
2024-04-02 10:26:06 +0200

    Add 05_test_recipe

---
+++ 05_test_recipe/ASSIGNMENT.md
@@ -0,0 +1,8 @@
+# Situation
+
+We want to write some unit tests our declarative recipe to check if it behaves as expected
+
+## TODO 1
+
+- Add test dependencies to rewrite-java, rewrite-maven, rewrite-java-dependencies, rewrite-java-21
+- Fill out unit test skeleton MyRecipesTest
---
+++ 05_test_recipe/src/test/java/de/openvalue/openrewrite/recipe/MyRecipesTest.java
@@ -0,0 +1,44 @@
+package de.openvalue.openrewrite.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+
+public class MyRecipesTest implements RewriteTest {
+
+  @Override
+  public void defaults(RecipeSpec spec) {
+    // Add MyRecipes as recipe to spec (see RewriteTest.fromRuntimeClasspath to load a declarative recipe)
+  }
+
+  @Test
+  void doesRenamePackage() {
+    // Create a test with a Java Spec with the package name that should be renamed
+  }
+
+  @Test
+  void doesNotRenameOtherPackage() {
+    // Create a test with a Java Spec with a different package name that should not be renamed
+  }
+
+  @Test
+  void doesNotDowngradeSpringBootVersion() {
+    // create a test with a POM Spec (see org.openrewrite.maven.Assertions.pomXml) with a newer version of Spring Boot than defined in the recipe
+  }
+
+  @Test
+  void doesUpgradeSpringBootVersion() {
+    // create a test with a POM Spec with an older Spring Boot version (e.g. 3.1.10) to be upgraded to a newer one
+  }
+
+  @Test
+  void doesRemoveUnusedImports() {
+    // Create a test with a Java Spec with an used import
+  }
+
+  @Test
+  void doesNotRemoveUsedImports() {
+    // Create a test with a Java Spec (see org.openrewrite.java.Assertions.java) with an import that is actually used and should not be changed
+  }
+}
