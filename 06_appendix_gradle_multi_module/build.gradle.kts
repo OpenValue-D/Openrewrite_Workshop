e08b413a1fbdf51d662e6d702c79430023b2ce5b
Sebastian Konieczek <sebastian.konieczek@openvalue.de>
2024-04-05 18:09:11 +0200

    add solution

--- 06_appendix_gradle_multi_module/build.gradle.kts
+++ 06_appendix_gradle_multi_module/build.gradle.kts
@@ -2,16 +2,14 @@ plugins {
    java
    `jvm-test-suite`
    idea
+   id("org.openrewrite.rewrite") version "6.11.2"
 }
-
-
 
 allprojects {
    repositories {
       mavenCentral()
       mavenLocal()
    }
-
 }
 
 idea {
@@ -19,4 +17,13 @@ idea {
       isDownloadJavadoc = true
       isDownloadSources = true
    }
+}
+
+dependencies {
+   rewrite(platform("org.openrewrite.recipe:rewrite-recipe-bom:2.9.0"))
+   rewrite("org.openrewrite.recipe:rewrite-testing-frameworks:2.6.0")
+}
+
+rewrite {
+   activeRecipe("de.openvalue.workshop.GradleMultiModuleFix")
 }
