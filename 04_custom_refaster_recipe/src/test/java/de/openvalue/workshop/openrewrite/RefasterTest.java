3bbfd8efdf5ac677581deb7ba9acecdd1a980642
Sascha Selzer <sascha.selzer@gmail.com>
2024-05-07 20:56:12 +0200

    Add maven to 04_custom_refaster_recipe

--- 04_custom_refaster_recipe/src/test/java/de/openvalue/workshop/openrewrite/RefasterTest.java
+++ 04_custom_refaster_recipe/src/test/java/de/openvalue/workshop/openrewrite/RefasterTest.java
@@ -5,29 +5,26 @@ import static org.openrewrite.java.Assertions.java;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RewriteTest;
 
-public class RefasterTest
-   implements RewriteTest
-{
-   @Test
-   void testRecipe()
-   {
-      rewriteRun( java( """
-                        package my.test;
-                                                
-                        class TestClass {
-                           public boolean isEven(int number) {
-                              return number % 2 == 0 ? true : false;
-                           }
-                        }
-                        """,
-                        """
-                        package my.test;
-                                                
-                        class TestClass {
-                           public boolean isEven(int number) {
-                              return number % 2 == 0;
-                           }
-                        }
-                        """ ) );
-   }
+public class RefasterTest implements RewriteTest {
+  @Test
+  void testRecipe() {
+    rewriteRun(java("""
+        package my.test;
+                                
+        class TestClass {
+           public boolean isEven(int number) {
+              return number % 2 == 0 ? true : false;
+           }
+        }
+        """,
+      """
+        package my.test;
+                                
+        class TestClass {
+           public boolean isEven(int number) {
+              return number % 2 == 0;
+           }
+        }
+        """));
+  }
 }
